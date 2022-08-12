0bb21fc53f7d2d26ff3fce4cb367f1d19724547b
Roy <royaldsouza150@gmail.com>
2022-08-12 10:55:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("This is my tutorial");
+
+const imgBox = document.querySelector('.imgBox');
+const whiteBoxes = document.getElementsByClassName('whiteBox');
+
+//Event listeners for dragable element imgBox
+imgBox.addEventListener('dragstart', (e) => {
+    console.log('DragStart has been triggered');
+    e.target.className += ' hold';
+    setTimeout(() => {
+        e.target.className = ' hide';
+    }, 0);
+});
+
+imgBox.addEventListener('dragend', (e) => {
+    console.log('DragEnd has been triggered');
+    e.target.className = 'imgBox';
+});
+
+
+
+for (whiteBox of whiteBoxes) {
+    whiteBox.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        console.log('DragOver has been triggered');
+    });
+    whiteBox.addEventListener('dragenter', (e) => {
+        console.log('DragEnter has been triggered');
+        e.target.className += ' dashed';
+    });
+    whiteBox.addEventListener('dragleave', (e) => {
+        console.log('Dragleave has been triggered');
+        e.target.className = 'whiteBox';
+    });
+    whiteBox.addEventListener('drop', (e) => {
+        console.log('Drop has been triggered');
+        e.target.append(imgBox)
+    });
+}
